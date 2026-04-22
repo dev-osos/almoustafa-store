bfdbd373eb0d06cc86a33f17bcec6868c3865338
osama <osama@osamas-Air.home>
2026-04-22 17:46:21 +0200

    Update - 2026-04-22 17:46

--- sw.js
+++ sw.js
@@ -23,6 +23,7 @@ const PRECACHE_ASSETS = [
   'js/cookie-consent.js'
 ];
 
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_ASSETS))
